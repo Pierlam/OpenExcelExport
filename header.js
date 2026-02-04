80c8366b5960d6ed48336fff19eeafb9f6e6a15c
pierlam-project <pierlam-project@outlook.com>
2026-02-04 21:01:17 +0100

    header reworked

--- header.js
+++ header.js
@@ -1,15 +1,16 @@
 document.write(`	
-	<div class="container my-4 ">
-		<div class="row justify-content-center">
-			<div class="col-lg mb-lg-4">
-				<img src="images/excel_banner.jpg" width="400" class="img-fluid" alt="" srcset="">
-			</div>
-			<div class="col-lg mx-2 align-self-center">
-				<div class="my-3">
-					<h1 class="text-center">OpenExcelExport</h1>
-					<p>OpenExcelExport is a basic but useful open-source application to extract main data from an excel: Shared strings, Styles/CellFormats, Fills, Borders and Fonts.</p>
-				</div>
-			</div>
-		</div>
-	</div>
+
+  <div class="container p-3 my-3 bg-image" style="
+      background-image: url('images/excel_banner.jpg');
+      height: 200px;
+    ">
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-center text-white">
+          <h1 class="mb-3">OpenExcelExport</h1>
+			<p>OpenExcelExport is a basic but useful open-source application to extract main data from an excel: Shared strings, Styles/CellFormats, Fills, Borders and Fonts.</p>
+        </div>
+      </div>
+    </div>
+  </div>  
 `);
